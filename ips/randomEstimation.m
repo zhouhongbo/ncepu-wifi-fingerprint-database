bdd49f0e1c8d61b2a10a33d70a6b8492b6870925
zhou_hong_bo <15650798020@163.com>
2020-10-04 22:28:26 +0800

    Update randomEstimation.m
    修复错误

--- ips/randomEstimation.m
+++ ips/randomEstimation.m
@@ -6,7 +6,7 @@ function [estimates] = randomEstimation(samples, query, positions, k)
     estimates = zeros(nQuery,3);
     
     for i = (1:nQuery)
-        ns = neighbors(1,:);
+        ns = neighbors(i,:);
         estimates(i,1) = mean(pos(ns,1));
         estimates(i,2) = mean(pos(ns,2));
         estimates(i,3) = mode(pos(ns,3));
