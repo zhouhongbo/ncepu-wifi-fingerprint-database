63e2e01deffca3b3cacf68dc247543d6b5bc7620
zhou_hong_bo <15650798020@163.com>
2020-10-04 22:32:32 +0800

    Update Charting.m
    使代码适应新地图

--- Charting.m
+++ Charting.m
@@ -1,19 +1,23 @@
 % Script to generate the paper's figure "Charting3rd" and "Charting5th"
 
-close all;
+chosenAP = 3; % 选哪个AP的数据，范围[1, 10]
 
+close all;
 addpath('db','files','ids','ips');
 
-apNumber = 7;
+ap24 = chosenAP * 2 - 1;
+ap5 = chosenAP * 2;
 barWidth = 0.25;
 
-data = loadContentSpecific('db', 1, 1, 15);
+data = loadContentSpecific('db', 2, [2, 4, 6, 8], 1); % 所有训练集
 data.rss(data.rss==100) = nan;
 
-[M, S, pos] = getMeanAndStd(data.rss(:,apNumber),data.coords); % 12 samples considered per location
+% 2.4g频率
+[M, S, pos] = getMeanAndStd(data.rss(:,ap24),data.coords); % 验证过没错
+drawRssBars(pos(:,1), pos(:,2), barWidth, M(:,:), S(:,:), ['AP', num2str(chosenAP), ' 2.4g']);
+set(gca,'XDir','reverse'); %将x轴方向设置为反向(从右到左递增)
 
-thirdFloorInds = pos(:,3) == 3;
-drawRssBars(pos(thirdFloorInds,1), pos(thirdFloorInds,2), barWidth, M(thirdFloorInds,:), S(thirdFloorInds,:), '3rd Floor');
-
-fifthFloorInds = pos(:,3) == 5;
-drawRssBars(pos(fifthFloorInds,1), pos(fifthFloorInds,2), barWidth, M(fifthFloorInds,:), S(fifthFloorInds,:), '5th Floor');
+% 5g频率
+[M, S, pos] = getMeanAndStd(data.rss(:,ap5),data.coords); % 12 samples considered per location
+drawRssBars(pos(:,1), pos(:,2), barWidth, M(:,:), S(:,:), ['AP', num2str(chosenAP), ' 5g']);
+set(gca,'XDir','reverse'); %将x轴方向设置为反向(从右到左递增)
